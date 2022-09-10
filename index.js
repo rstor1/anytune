36ae7f0bb381a24c072bb9c04c4cc79038b917ba
Ryan Storm <ryanstorm@Ryans-MacBook-Pro.local>
2022-09-10 15:09:20 -0400

    First working track

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ import TrackPlayer from 'react-native-track-player';
 
 AppRegistry.registerComponent(appName, () => App);
 TrackPlayer.registerPlaybackService(() => require('./service'));
+
+// Setup trackplayer. Doing this in the player component causes errors because each navigation
+// calls setup again.
+const setUpTrackPlayer = async () => {
+    try {
+      await TrackPlayer.setupPlayer({});
+      console.log('TrackPlayer initialized.');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+
+setUpTrackPlayer();
